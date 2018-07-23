638ae8b95f23e18d160e44aa63702f75c7627919
pauloheck <pauloheck@gmail.com>
2018-07-23 15:39:45 -0300

    ajustes

--- src/main/java/br/com/tt/util/ConvertTest.java
+++ src/main/java/br/com/tt/util/ConvertTest.java
@@ -1,15 +1,15 @@
-package br.com.tt.util;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class ConvertTest {
-
-	//@Test
-	public void testCpf() {
-		String cpf = new Convert().cpf("03279245691");
-		assertEquals("032.792.456-91", cpf);
-	}
-
-}
+//package br.com.tt.util;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//public class ConvertTest {
+//
+//	@Test
+//	public void testCpf() {
+//		String cpf = new Convert().cpf("03279245691");
+//		assertEquals("032.792.456-91", cpf);
+//	}
+//
+//}
