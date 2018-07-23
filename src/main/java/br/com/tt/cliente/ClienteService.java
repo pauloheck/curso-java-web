bf9c17ab885cb260a6a842c026d6cf9c832bbc3b
pauloheck <pauloheck@gmail.com>
2018-07-23 16:41:33 -0300

    ajustes

--- src/main/java/br/com/tt/cliente/ClienteService.java
+++ src/main/java/br/com/tt/cliente/ClienteService.java
@@ -29,24 +29,25 @@ public class ClienteService {
 	}
 
 	public Cliente buscar(Long id) {
-		return repository.findOne(id);
+		// return repository.findOne(id);
+		return repository.findById(id).orElse(null);
 	}
 
 	public void excluir(Long id) {
-		repository.delete(id);
+//		repository.delete(id);
+		repository.deleteById(id);
 
 	}
 
 	public CadastroReceita consultaReceita(String cnpj) throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
-String url = "https://www.receitaws.com.br/v1/cnpj/27865757000102";
+		String url = "https://www.receitaws.com.br/v1/cnpj/27865757000102";
 		String receitaJson = HttpClient.get(url);
-CadastroReceita cadReceita = 
-mapper.readValue(receitaJson, CadastroReceita.class);
-System.out.println(cadReceita.getFantasia());
+		CadastroReceita cadReceita = mapper.readValue(receitaJson, CadastroReceita.class);
+		System.out.println(cadReceita.getFantasia());
 		return null;
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		new ClienteService().consultaReceita("");
 	}
