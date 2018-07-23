39b7da6843507405e1146031baec957ee9c980b2
pauloheck <pauloheck@gmail.com>
2018-07-23 15:53:06 -0300

    ajustes

--- src/main/java/br/com/tt/cliente/ClienteController.java
+++ src/main/java/br/com/tt/cliente/ClienteController.java
@@ -20,7 +20,7 @@ public class ClienteController {
 
 	@GetMapping
 	ModelAndView cliente() {
-		ModelAndView mv = new ModelAndView("cliente/cliente.html");
+		ModelAndView mv = new ModelAndView("cliente/index");
 		mv.addObject("clientes", service.consulta());
 		mv.addObject("cpf", "11111111111");
 		return mv;
